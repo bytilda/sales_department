8bb01e140dfeeacbe7bd692a4709438f0a78fdf1
MrsDramaqueen <chelyaev_99@mail.ru>
2022-04-21 02:31:55 +0700

    Добавил в БД товары

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -33,4 +33,34 @@ insert into okei (units) values ('т');
 insert into okei (units) values ('шт');
 insert into okei (units) values ('упак');
 
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024125116, Хромовая листовая лента 1х20х5000, 15, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024122116, Хромовая листовая лента 1х10х2500, 7, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024124226, Хромовая листовая лента 1х13х3000, 10, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024122466, Хромовая листовая лента 1х10х2750, 7.5, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024121786, Хромовая листовая лента 1х5х1500, 3, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024129446, Хромовая листовая лента 1х25х7500, 25, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024345212, Стальная опорная балка 1000х500х4000, 45, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024349222, Стальная опорная балка 2000х1000х6000, 105, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024343782, Стальная опорная балка 500х500х4000, 20, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024346772, Стальная опорная балка 1000х500х4500, 60, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024346782, Стальная опорная балка 1000х500х4000, 55, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024348812, Стальная опорная балка 1000х1000х6000, 85, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024241343, Аллюминевый лист 1х500х1000, 4, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024241223, Аллюминевый лист 1х100х500, 1, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024249773, Аллюминевый лист 1х1000х3000, 25, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024245643, Аллюминевый лист 1х1000х1000, 10, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024471557, Готовая металлическая конструкция "КОТ" 3000х1500х5000, 1, т');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024891778, Готовая металлическая конструкция "РАКЕТА" 800х800х2000, 100, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024771565, Готовая металлическая конструкция "ПАЛЬМА" 800х800х2000, 90, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024771565, Готовая металлическая конструкция "ВВЕРХ" 3000х2500х1500, 40, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820014758314, Аллюминевое основание лампы "ГЕЙША", 5, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820078924544, Аллюминевое основание лампы "АФРОДИТА", 4, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820035648974, Аллюминевое основание лампы "РОССИЯ", 8, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820045213784, Аллюминевое основание лампы "ДИПЛОМ", 1, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820013246844, Аллюминевое основание лампы "ТОННЕЛЬ", 5, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820097856424, Аллюминевое основание лампы "ПРОМЕТЕЙ", 7, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820045632179, Чугунный Бюст Человека 150х150х300, 7, кг');
+insert into product_nomenclature (product_cipher, product_name, weight, units) values ('4820024111111, Чугунный Кубик Рубика 56х56х56, 1, кг');
+
+
 
